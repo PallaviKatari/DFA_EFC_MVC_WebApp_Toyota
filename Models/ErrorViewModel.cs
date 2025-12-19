60b6f77ad1d0c458158e3bdfa500a7c5eede197a
PallaviKatari <pallavikatari@gmail.com>
2025-12-19 09:51:31 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DFA_EFC_MVC_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Product1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;//Validation attributes
+
+namespace DFA_EFC_MVC_WebApp.Models;
+
+public partial class Product1
+{
+    public int ProductId { get; set; }
+
+    //[] are called as Data Annotations or Attributes
+    [Required(ErrorMessage = "Product name is required")]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    [Range(1, 100000, ErrorMessage = "Price must be greater than 0")]
+    public decimal Price { get; set; }
+
+    [Required]
+    [Range(0, 10000)]
+    public int Quantity { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+}
